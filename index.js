c884dcb8cf5cb3a06617012132c7ee237d4b6bca
Kanon Kakuno <yadex205@outlook.jp>
2017-06-02 10:20:48 +0900

    :tada: all path-related methods implemented!

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ function Pathname(filepath) {
 }
 
 Pathname.prototype = {
+  ascend: function() {
+    let result = []
+    let currentPath = this
+    let sepCount = (this.toString().match(new RegExp(path.sep, 'g')) || []).length + 1
+    for (; sepCount > 0; sepCount--) {
+      result.push(currentPath)
+      if (currentPath.isRoot()) { break }
+      currentPath = currentPath.parent()
+    }
+    return result
+  },
+
   basename: function(ext) {
     return new Pathname(path.basename(this.toString(), ext))
   },
@@ -15,8 +27,22 @@ Pathname.prototype = {
     return new Pathname(path.normalize(this.toString()))
   },
 
+  descend: function() {
+    return this.ascend().reverse()
+  },
+
   dirname: function() {
     return new Pathname(path.dirname(this.toString()))
+  },
+
+  eachFilename: function(callback) {
+    this.toString().split(path.sep).forEach(filepath => {
+      if (filepath) { callback(filepath) }
+    })
+  },
+
+  expandPath: function(defaultDir) {
+    return new Pathname(path.resolve(defaultDir || '.', this.toString()))
   },
 
   extname: function() {
@@ -31,9 +57,33 @@ Pathname.prototype = {
     return !path.isAbsolute(this.toString())
   },
 
+  isRoot: function() {
+    return this.cleanpath().toString() === path.parse(this.toString()).root
+  },
+
   join: function() {
     let args = Array.from(arguments)
     return new Pathname(path.join.apply(this, [this.toString()].concat(args)))
+  },
+
+  parent: function() {
+    return this.join('..')
+  },
+
+  relativePathFrom: function(base) {
+    return new Pathname(path.relative(base.toString(), this.toString()))
+  },
+
+  split: function() {
+    return [this.dirname(), this.basename()]
+  },
+
+  sub: function(pattern, replace) {
+    return new Pathname(this.toString().replace(pattern, replace))
+  },
+
+  subExt: function(ext) {
+    return this.dirname().join(this.basename(this.extname()).toString() + ext)
   },
 
   toString: function() {
--- test/pathname_spec.js
+++ test/pathname_spec.js
@@ -2,6 +2,42 @@ const expect = require('chai').expect
 const Pathname = require('..')
 
 describe('Pathname', () => {
+  'use strict'
+
+  describe('#ascend', () => {
+    context('when /foo/bar/baz', () => {
+      let pathname = new Pathname('/foo/bar/baz')
+      let subject = pathname.ascend().map(val => val.toString())
+
+      it ('should be ["/foo/bar/baz", "/foo/bar", "/foo", "/"]', () => {
+        expect(subject).to.eql(['/foo/bar/baz', '/foo/bar', '/foo', '/'])
+      })
+    })
+
+    context('when foo/bar/baz', () => {
+      let pathname = new Pathname('foo/bar/baz')
+      let subject = pathname.ascend().map(val => val.toString())
+
+      it('should be ["foo/bar/baz", "foo/bar", "foo"]', () => {
+        expect(subject).to.eql(['foo/bar/baz', 'foo/bar', 'foo'])
+      })
+    })
+
+    context('when path is "."', () => {
+      let pathname = new Pathname('.')
+      it('should be ["."]', () => {
+        expect(pathname.ascend().map(val => val.toString())).to.eql(['.'])
+      })
+    })
+
+    context('when path is "/"', () => {
+      let pathname = new Pathname('/')
+      it('should be ["/"]', () => {
+        expect(pathname.ascend().map(val => val.toString())).to.eql(['/'])
+      })
+    })
+  })
+
   describe('#basename', () => {
     it('should return "bar"', () => {
       expect(new Pathname('foo/bar').basename().toString()).to.eql('bar')
@@ -18,9 +54,97 @@ describe('Pathname', () => {
     })
   })
 
+  describe('#descend', () => {
+    context('when /foo/bar/baz', () => {
+      let pathname = new Pathname('/foo/bar/baz')
+      let subject = pathname.descend().map(val => val.toString())
+
+      it ('should be ["/", "/foo", "/foo/bar", "/foo/bar/baz"]', () => {
+        expect(subject).to.eql(['/', '/foo', '/foo/bar', '/foo/bar/baz'])
+      })
+    })
+
+    context('when foo/bar/baz', () => {
+      let pathname = new Pathname('foo/bar/baz')
+      let subject = pathname.descend().map(val => val.toString())
+
+      it('should be ["foo", "foo/bar", "foo/bar/baz"]', () => {
+        expect(subject).to.eql(['foo', 'foo/bar', 'foo/bar/baz'])
+      })
+    })
+
+    context('when path is "."', () => {
+      let pathname = new Pathname('.')
+      it('should be ["."]', () => {
+        expect(pathname.descend().map(val => val.toString())).to.eql(['.'])
+      })
+    })
+
+    context('when path is "/"', () => {
+      let pathname = new Pathname('/')
+      it('should be ["/"]', () => {
+        expect(pathname.descend().map(val => val.toString())).to.eql(['/'])
+      })
+    })
+  })
+
   describe('#dirname', () => {
     it('should return "foo"', () => {
       expect(new Pathname('foo/bar').dirname().toString()).to.eql('foo')
+    })
+  })
+
+  describe('#eachFilename', () => {
+    context('when absolute path given', () => {
+      let pathname = new Pathname('/foo/bar')
+      it('should makes result to be ["foo", "bar"]', () => {
+        let result = []
+        pathname.eachFilename(filepath => result.push(filepath))
+        expect(result).to.eql(['foo', 'bar'])
+      })
+    })
+
+    context('when relative path given', () => {
+      let pathname = new Pathname('foo/bar')
+      it('should makes result to be ["foo", "bar"]', () => {
+        let result = []
+        pathname.eachFilename(filepath => result.push(filepath))
+        expect(result).to.eql(['foo', 'bar'])
+      })
+    })
+
+    context('when "/" givn', () => {
+      let pathname = new Pathname('/')
+      it('should make result to be empty', () => {
+        let result = []
+        pathname.eachFilename(filepath => result.push(filepath))
+        expect(result).to.be.empty
+      })
+    })
+
+    context('when "." givn', () => {
+      let pathname = new Pathname('.')
+      it('should make result to be ["."]', () => {
+        let result = []
+        pathname.eachFilename(filepath => result.push(filepath))
+        expect(result).to.eql(['.'])
+      })
+    })
+  })
+
+  describe('#expandPath', () => {
+    context('with no defaultDir', () => {
+      let pathname = new Pathname('foo/bar')
+      it('should return an absolute path', () => {
+        expect(pathname.expandPath().toString()).to.eql(process.cwd() + '/foo/bar')
+      })
+    })
+
+    context('with defaultDir', () => {
+      let pathname = new Pathname('foo/bar')
+      it('should return an absolute path', () => {
+        expect(pathname.expandPath('/hoge').toString()).to.eql('/hoge/foo/bar')
+      })
     })
   })
 
@@ -48,7 +172,25 @@ describe('Pathname', () => {
     it('should be falsey', () => {
       expect(new Pathname('/foo').isRelative()).to.be.false
     })
-})
+  })
+
+  describe('#isRoot', () => {
+    it('should be truthy', () => {
+      expect(new Pathname('/').isRoot()).to.be.true
+    })
+
+    it('should be falsey', () => {
+      expect(new Pathname('/foo').isRoot()).to.be.false
+    })
+
+    it('should be falsey', () => {
+      expect(new Pathname('foo').isRoot()).to.be.false
+    })
+
+    it('should be falsey', () => {
+      expect(new Pathname('.').isRoot()).to.be.false
+    })
+  })
 
   describe('#join', () => {
     it('should return "foo"', () => {
@@ -61,6 +203,40 @@ describe('Pathname', () => {
 
     it ('should return "foo"', () => {
       expect(new Pathname('foo/bar').join('..').toString()).to.eql('foo')
+    })
+  })
+
+  describe('#parent', () => {
+    it('should return "foo"', () => {
+      expect(new Pathname('foo/bar').parent().toString()).to.eql('foo')
+    })
+
+    it('should returl "/"', () => {
+      expect(new Pathname('/foo').parent().toString()).to.eql('/')
+    })
+  })
+
+  describe('#relativePathFrom', () => {
+    let pathname = new Pathname('/hoge/foo/bar')
+    let base = new Pathname('/hoge')
+    it('should return a relative path', () => {
+      expect(pathname.relativePathFrom(base).toString()).to.eql('foo/bar')
+    })
+  })
+
+  describe('#subExt', () => {
+    context('when call #subExt for the path with extension', () => {
+      let pathname = new Pathname('/foo.txt')
+      it('should return a valid path', () => {
+        expect(pathname.subExt('.csv').toString()).to.eql('/foo.csv')
+      })
+    })
+
+    context('when call #subExt for the path with no extension', () => {
+      let pathname = new Pathname('/foo')
+      it('should return a valid path', () => {
+        expect(pathname.subExt('.csv').toString()).to.eql('/foo.csv')
+      })
     })
   })
 
